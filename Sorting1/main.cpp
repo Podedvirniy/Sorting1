7a44e5152627b27d37c6aa7e5ed6b88398442222
Podedvirniy <Podedvirniy16@gmail.com>
2022-06-02 16:00:32 +0300

    Sorting1

--- Sorting1/main.cpp
+++ Sorting1/main.cpp
@@ -1,14 +1,64 @@
-//
-//  main.cpp
-//  Sorting1
-//
-//  Created by Сергій Подедвірний on 02.06.2022.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <ctime>
+ 
+using namespace std;
+ 
+void FillArray(int arr[], int size)
+{
+ for (int i = 0; i < size; i++)
+ {
+  arr[i] = rand() % 100000;
+ 
+ }
+}
+ 
+void ShowArray(int arr[], int size)
+{
+ cout << endl;
+ for (int i = 0; i < size; i++)
+ {
+  cout << arr[i] << ' ';
+ 
+ }
+ cout << endl;
+}
+ 
+ 
+void Choose(int arr[], int size)
+{
+ int lowindex, lowkey;
+ 
+ 
+ 
+ for (int i = 0; i < (size - 1); i++) {
+ 
+  lowindex = i;
+ 
+  lowkey = arr[i];
+ 
+  for (int j = i + 1; j < size; j++) {
+ 
+   if (arr[j] < lowkey) {
+ 
+    lowkey = arr[j];
+ 
+    lowindex = j;
+ 
+   }
+  }
+  swap(arr[i], arr[lowindex]);
+ 
+ }
+}
+ 
+int main()
+{
+ srand(time(NULL));
+ int arr[100];
+ FillArray(arr, 100);
+ ShowArray(arr, 100);
+ 
+ Choose(arr, 100);
+ ShowArray(arr, 100);
+ 
 }
